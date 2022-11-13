19e36ad420a40b953f84886afeaae78229020608
Dorina <acsinoi.dorina@yandex.ru>
2022-11-13 18:28:41 +0300

    example

---
+++ Example003/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Программа должна сложить два целых числа. */
+Console.WriteLine("Введите первое число!");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число!");
+int num2 = Convert.ToInt32( Console.ReadLine());
+
+int resulte = num1 + num2;
+Console.WriteLine (resulte);
