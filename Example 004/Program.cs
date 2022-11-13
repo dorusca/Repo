711f10595aece5538efa6d792b50fcd484120103
Dorina <acsinoi.dorina@yandex.ru>
2022-11-13 18:44:06 +0300

    example 004

---
+++ Example 004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти частное двух чисел. Вместо int пишем double, чтобы деление было с остатком
+//
+
+Console.WriteLine("Введите первое число: ");
+double num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+double num2 = Convert.ToInt32( Console.ReadLine());
+
+
+double resulte = num1 / num2;
+
+Console.WriteLine(resulte);
