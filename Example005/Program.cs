cb97513c5f0c9d861c75a2a502dc80f93be1fe51
Dorina <acsinoi.dorina@yandex.ru>
2022-11-13 18:51:30 +0300

    example 005

---
+++ Example005/Program.cs
@@ -0,0 +1,9 @@
+﻿// выполнить сумму случайных, рандомных, чисел.
+int number1 = new Random().Next (1,20);
+Console.WriteLine(number1);
+
+int number2 = new Random().Next (1,20);
+Console.WriteLine(number2);
+
+int resulte = number1+ number2 ;
+Console.WriteLine(resulte);
