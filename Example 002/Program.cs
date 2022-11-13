9ef34c138564ab8e46de40b8b473c214c47d0714
Dorina <acsinoi.dorina@yandex.ru>
2022-11-13 17:58:33 +0300

    example 002

---
+++ Example 002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите ваше имя: ");
+string username = Console.ReadLine ();
+
+if (username == "маша")
+{
+    Console.WriteLine ("Ура, это же Мария!");
+}
+else 
+{
+Console.Write("Привет, ");
+Console.WriteLine (username);
+}
+
